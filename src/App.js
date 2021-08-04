ec47e5b90f8910fd2d9fc256ffda17537c5a4690
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-04 16:54:07 -0700

    added updated cover image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
 
 export default App;
+
