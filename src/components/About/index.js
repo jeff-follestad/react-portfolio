f72b0fd583efafb7dd4255fea91e7b5733306614
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-04 17:11:08 -0700

    updated about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ import coverImage from "../../assets/cover/cover-image.jpg";
 function About() {
   return (
 <section className="my-5">
-  <h1 id="about">Who am I?</h1>
+  <h1 id="about">Jeff Follestad</h1>
   <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
 </section>
   );
