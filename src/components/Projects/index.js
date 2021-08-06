572fcdd57c1bf82dc42ca1d49a4545cbf2b9da37
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-06 13:26:12 -0700

    modified projects/index.js

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,33 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
-
-function Projects() {
-  return (
-    <section>
-      <h1>NAME</h1>
-      <p>DESCRIPTION</p>
-=======
+import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
 
 function Projects(props) {
-  const currentCategory = {
-    name: "commercial",
-    description: 
-    "Photos of grocery stores and whatnot...",
-  };
+  const { currentCategory } = props;
   return (
     <section>
-      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-      <p>{currentCategory.Category.name}</p>
-      <div>
-        <img
-          src={photo}
-          alt="Commercial Example"
-          className="img-thumbnail mx-1"
-        />
-      </div>
->>>>>>> c976524aa1f047d1b83e1e1d6e2d8b529f6bcb6e
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
     </section>
   );
 }
