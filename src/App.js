5ad0e2d22b73d2d015a089636bb2f6cf9ebed64c
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-06 13:59:50 -0700

    trying a header layout I found online

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import React from "react";
 import Nav from "./components/Nav";
 import About from "./components/About";
-import Gallery from "./components/Projects";
-=======
-import React from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
-import Projects from './components/Projects';
->>>>>>> c976524aa1f047d1b83e1e1d6e2d8b529f6bcb6e
+import Projects from "./components/Projects";
 
 function App() {
 
