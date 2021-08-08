2295bcef9cbb66873d2d367d785b95a835b58e3e
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-08 13:23:27 -0700

    updating app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from "react";
-import Nav from "./components/Nav";
-import About from "./components/About";
-import Projects from "./components/Projects";
-
-function App() {
-
-  return (
-    <div>
-      <Nav></Nav>
-      <main>
-        <Projects></Projects>
-        <About></About>
-      </main>
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Testimonials from  './components/Testimonials';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <Testimonials resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
-
